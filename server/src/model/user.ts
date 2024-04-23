83add1c7053ab9227a33959b1407370b46c6b7d0
salidamaharjan <salidamaharjan@outlook.com>
2024-04-23 17:15:05 -0500

    Create Client model

---
+++ server/src/model/user.ts
@@ -0,0 +1,30 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../config/sequelizeConnection";
+
+class Client extends Model {}
+
+Client.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "client",
+  }
+);
+export default Client;
