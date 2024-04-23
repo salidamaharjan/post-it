fef24ad83d01bed007d0d21bc8c68ff2529e9b76
salidamaharjan <salidamaharjan@outlook.com>
2024-04-23 12:33:19 -0500

    Create a token for user

--- server/src/schema/resolvers.ts
+++ server/src/schema/resolvers.ts
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
 import Post from "../model/posts";
 import jwt from "jsonwebtoken";
+import { GraphQLError } from "graphql";
 
 type NewPost = {
   postName: String;
@@ -25,7 +28,23 @@ export const resolvers = {
       args: { username: String; password: String },
       __: any
     ) => {
-      return args.username;
+      if (args.username !== "adminu") {
+        throw new GraphQLError(
+          "You are not authorized to perform this action.",
+          {
+            extensions: { code: "Forbidden" },
+          }
+        );
+      } else {
+        const token = jwt.sign(
+          {
+            username: "adminu",
+          },
+          process.env.JWT_SECRET_KEY!,
+          { expiresIn: "1h" }
+        );
+        return token;
+      }
     },
   },
 };
